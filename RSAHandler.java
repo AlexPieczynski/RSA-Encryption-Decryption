82568d32c5cd1ecb078acbc80b5c12579d02f3e1
jduong321 <jduong2@uic.edu>
2016-03-11 01:24:03 -0600

    Update RSAHandler.java

--- RSAHandler.java
+++ RSAHandler.java
@@ -1,7 +1,3 @@
-/* class to create a public and private RSA key and save them to a file
- *   -also contains methods to encrypt/decrypt messages using the keys
- */
-
 public class RSAHandler
 {
   private HugePrime p;   
@@ -28,25 +24,40 @@ public class RSAHandler
    
     //how should the user enter the random values for e and d?
     //also have to use euclidean to find e. do we have to implement in HUI?
+    //or 3 for basic codes and 65537 (which is 216 + 1) for more secure codes. <-- this sounds like a good lazy way I do this
    
     HugeUnsignedInt k;
    
    
   }
  
+  
  
   //generates a public-private key set and saves each to a separate file
   public void generateKeys()
   {
+    //look up how to make pretty XML files. then public is e and n, private is d and n
   }
  
  
   public void blockFile(int blockSize, String fileName)
   {
+    /* First open the file and give it to a HUI
+     * mod the HUI by the blocksize to see how many 00 we need to add
+     * then use for loop till blocksize taking the thing
+     * to do this backwards have a for loop inside with the size of the number?
+     * then add to another file?
+     * 
+     **/
   }
  
   public void unblockFile(int blockSize, String fileName)
   {
+    /* When we read the line, the number is backwards. How to reverse it?
+     * -just put the line into a string then keep adding it then make a HUI
+     * WHAT IF WE HAVE LEADING 0 IN THE HUI ERROR
+     * 
+     * */
   }
  
   public void encrypt(String blockedFile, String keyFile, String outputFile)
